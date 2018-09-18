1a55d2da1689c3edf7e5ee7ad04f2e944545cd91
Lau Llobet <lau.llobet@codurance.com>
2018-09-18 19:07:47 +0200

    Routes Are circular

--- gossip/src/main/java/Route.java
+++ gossip/src/main/java/Route.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Route {
+    private int[] stops;
+    int currentStop = 0;
+    public Route(int...stops) {
+        this.stops = stops;
+    }
+
     public int getNextStop() {
-        throw new UnsupportedOperationException();
+        int ans = stops[currentStop];
+        currentStop++;
+        if(currentStop + 1 > stops.length){
+            currentStop = 0;
+        }
+        return ans;
     }
 }
