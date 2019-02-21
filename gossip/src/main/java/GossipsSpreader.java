ff7166c9dbc2b7288f07e7094f134d95af3e5e01
Lau Llobet <lau.llobet@gmail.com>
2019-02-21 13:40:56 +0100

    Refactor: remove coupling between to phases of the gossip spreader

--- gossip/src/main/java/GossipsSpreader.java
+++ gossip/src/main/java/GossipsSpreader.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 class GossipsSpreader {
 
-    private static final int NUMBER_OF_SHARED_GOSSIPS_THAT_GENERATES_A_NEW_ONE = 6;
+    private static final int AMOUNT_TO_GENERATE_A_NEW_GOSSIP = 6;
     private final List<BusStop> busStops;
 
     public GossipsSpreader(BusStop... stops) {
@@ -15,28 +16,33 @@ class GossipsSpreader {
     }
 
     private void spreadGossipsAt(BusStop busStop) {
-        Bus oneBusKnowingAllGossips;
-        oneBusKnowingAllGossips = shareAllGossipsBetweenBusesAt(busStop);
-        if(oneBusKnowingAllGossips != null &&
-                oneBusKnowingAllGossips.knowsNGossips(NUMBER_OF_SHARED_GOSSIPS_THAT_GENERATES_A_NEW_ONE)){
-            generateAndShareNewGossip(busStop, oneBusKnowingAllGossips);
-        }
+        shareAllGossipsBetweenBusesAt(busStop);
+
+        generateANewGossipAndSpreadItIfNecesary(busStop);
     }
 
-    private Bus shareAllGossipsBetweenBusesAt(BusStop busStop) {
-        Bus lastGossipListenerBus = null;
+    private void shareAllGossipsBetweenBusesAt(BusStop busStop) {
         for(Bus gossipListenerBus : busStop.getBuses() ){
             for(Bus gossipTellerBus: busStop.getBuses()){
                 gossipListenerBus.getToKnowGossipsFrom(gossipTellerBus);
-                lastGossipListenerBus = gossipListenerBus;
             }
         }
-        return lastGossipListenerBus;
     }
 
-    private void generateAndShareNewGossip(BusStop busStop, Bus lastGossipListenerBus) {
-        lastGossipListenerBus.createAGossip();
-        spreadGossipsAt(busStop);
+    private void generateANewGossipAndSpreadItIfNecesary(BusStop busStop) {
+        Bus randomBus = getARandomBusFrom(busStop);
+        if(randomBus != null && randomBus.knowsNGossips(AMOUNT_TO_GENERATE_A_NEW_GOSSIP)){
+            randomBus.createAGossip();
+            spreadGossipsAt(busStop);
+        }
+    }
+
+    private Bus getARandomBusFrom(BusStop busStop) {
+        Iterator<Bus> iterator = busStop.getBuses().iterator();
+        if(iterator.hasNext()){
+            return iterator.next();
+        }
+        return null;
     }
 
 }
